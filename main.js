d5b46216ec506855cc6afb9cf956476b4ee826ea
Kevin Marcelo Lopez Tordoya <kevin.inck@gmail.com>
2023-11-07 22:04:41 -0400

    stash command

--- main.js
+++ main.js
@@ -3,4 +3,8 @@ var clicks = 0;
 function onClick() {
   clicks += 1;
   document.getElementById("clicks").innerHTML = clicks;
-};
+}
+function Dividir() {
+  clicks += 1;
+  document.getElementById("clicks").innerHTML = clicks;
+}
